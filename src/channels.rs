b47c4ce817dc038626c993768750a12c1cf34d79
Mike Rettig <mrettig@Mikes-MacBook-Pro.local>
2015-07-05 10:10:45 -0500

    pass closures

--- src/channels.rs
+++ src/channels.rs
@@ -4,11 +4,15 @@ use std::thread;
 
 static NTHREADS: i32 = 3;
 
+trait Event : Sized {
+    fn run();
+}
+
 fn main() {
     // Channels have two endpoints: the `Sender<T>` and the `Receiver<T>`,
     // where `T` is the type of the message to be transferred
     // (type annotation is superfluous)
-    let (tx, rx): (Sender<i32>, Receiver<i32>) = mpsc::channel();
+    let (tx, rx): (Sender<Box<Fn()+Send>>, Receiver<Box<Fn()+Send>>) = mpsc::channel();
 
     for id in 0..NTHREADS {
         // The sender endpoint can be copied
@@ -16,24 +20,23 @@ fn main() {
 
         // Each thread will send its id via the channel
         thread::spawn(move || {
+            let printer = move || println!("{:?}", id);
             // The thread takes ownership over `thread_tx`
             // Each thread queues a message in the channel
-            thread_tx.send(id).unwrap();
+            thread_tx.send(Box::new(printer)).unwrap();
 
             // Sending is a non-blocking operation, the thread will continue
             // immediately after sending its message
-            println!("thread {} finished", id);
+            //println!("thread {} finished", id);
         });
     }
 
-    // Here, all the messages are collected
-    let mut ids = Vec::with_capacity(NTHREADS as usize);
     for _ in 0..NTHREADS {
         // The `recv` method picks a message from the channel
         // `recv` will block the current thread if there no messages available
-        ids.push(rx.recv());
+        rx.recv().unwrap()();
     }
 
     // Show the order in which the messages were sent
-    println!("{:?}", ids);
+    //println!("{:?}", ids);
 }
